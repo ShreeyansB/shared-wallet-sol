aeca8398e67066f3d880b4b9628dc618877ffcd7
ShreeyansB <sbahadkar@rocketmail.com>
2022-01-20 18:08:16 +0530

    :tada: Added basic ownership and withdrawal code

---
+++ SharedWallet.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.11;
+// SPDX-License-Identifier: MIT
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract SharedWallet is Ownable {
+
+    mapping(address => uint) public allowance;
+
+    receive() external payable {
+
+    }
+
+    function addAllowance(address _to, uint _amount) public onlyOwner {
+        allowance[_to] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+    require(msg.sender == owner() || allowance[msg.sender] >= _amount, "You are not allowed.");
+    _;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        _to.transfer(_amount);
+    }    
+
+}
+
