ad07a62bec0cc55a0359fc1821b368a8951b4ce1
ShreeyansB <sbahadkar@rocketmail.com>
2022-01-20 18:16:21 +0530

    :art: Improved structure of contract

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -3,25 +3,31 @@ pragma solidity ^0.8.11;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract SharedWallet is Ownable {
 
+contract Allowance is Ownable {
     mapping(address => uint) public allowance;
 
-    receive() external payable {
-
-    }
-
-    function addAllowance(address _to, uint _amount) public onlyOwner {
+    function setAllowance(address _to, uint _amount) public onlyOwner {
         allowance[_to] = _amount;
-    }
-
-    function reduceAllowance(address _to, uint _amount) internal ownerOrAllowed(_amount) {
-        allowance[_to] -= _amount;
     }
 
     modifier ownerOrAllowed(uint _amount) {
     require(msg.sender == owner() || allowance[msg.sender] >= _amount, "You are not allowed.");
     _;
+    }
+
+
+    function reduceAllowance(address _to, uint _amount) internal ownerOrAllowed(_amount) {
+        allowance[_to] -= _amount;
+    }
+
+
+}
+
+contract SharedWallet is Ownable, Allowance {
+
+    receive() external payable {
+
     }
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
